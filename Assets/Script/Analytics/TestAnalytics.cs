2e3557a7884ed5ecbed7e3fe609dadb6bb5dd637
tewlywth156 <takdanai.thada@gmail.com>
2022-09-12 11:57:31 +0700

    fix analytic

--- Assets/Script/Analytics/TestAnalytics.cs
+++ Assets/Script/Analytics/TestAnalytics.cs
@@ -1,38 +1,36 @@
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.Analytics;
 using System.Collections.Generic;
-using UnityEngine.Analytics;
 
 public class TestAnalytics : MonoBehaviour
 {
-    private void Start()
+    private async void Start()
     {
-        //testSendAnalyticEvent();
+        try
+        {
+            await UnityServices.InitializeAsync();
+            List<string> consentIdentifiers = await AnalyticsService.Instance.CheckForRequiredConsents();
+        }
+        catch (ConsentCheckException e)
+        {
+            // Something went wrong when checking the GeoIP, check the e.Reason and handle appropriately.
+        }
+
         testSendCustomEvent();
-    }
-
-    private void testSendAnalyticEvent()
-    {
-        Dictionary<string, object> customParams = new Dictionary<string, object>();
-        customParams.Add("MapId", "testMapId");
-        customParams.Add("TestScore", 10000);
-        customParams.Add("TestTime", Time.time);
-
-        var result = AnalyticsEvent.LevelComplete("test",customParams);
-
-        Debug.Log(result);
-
     }
 
     private void testSendCustomEvent()
     {
         var customParams = new Dictionary<string, object>();
-        customParams.Add("MapId", "testMapId");
-        customParams.Add("TestScore", 10000);
-        customParams.Add("TestTime", Time.time);
+        customParams.Add("mapId", "testMapId");
+        customParams.Add("score", 10000);
+        customParams.Add("time", Time.time);
 
-        var result = Analytics.CustomEvent("ChallengeModeEnd", customParams);
+        AnalyticsService.Instance.CustomData("ChallengeModeEnd", customParams);
 
-        Debug.Log(result);
+        AnalyticsService.Instance.Flush();
+        Debug.Log("Do");
     }
 
 }
