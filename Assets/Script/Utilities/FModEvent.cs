a63f672377cbfa9e189f69d100d0f40c3e66014f
tewlywth156 <takdanai.thada@gmail.com>
2023-03-06 23:40:01 +0700

    add walk and run SFX

--- Assets/Script/Utilities/FModEvent.cs
+++ Assets/Script/Utilities/FModEvent.cs
@@ -9,17 +9,8 @@ public class FModEvent : MonoBehaviour
     [field : Header("Player")]
     [field : SerializeField] public EventReference PlayerWalkSFX {get; private set;}
     [field : SerializeField] public EventReference PlayerRunSFX {get; private set;}
-    [field : SerializeField] public EventReference PlayerJumpSFX {get; private set;}
-    [field : SerializeField] public EventReference PlayerHitGroundSFX {get; private set;}
-    [field : SerializeField] public EventReference ShrinkSFX {get; private set;}
-    [field : SerializeField] public EventReference ExtendSFX {get; private set;}
-    
-    [field : SerializeField] public EventReference ClockSFX {get; private set;}
+
     
     [field : Header("UI")]
     [field : SerializeField] public EventReference ButtonSFX {get; private set;}
-
-    [field : SerializeField] public EventReference CutScene1 {get; private set;}
-    [field : SerializeField] public EventReference CutScene2 {get; private set;}
-
 }
