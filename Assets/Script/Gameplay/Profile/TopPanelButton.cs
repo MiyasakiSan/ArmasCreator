36725c9afa7813b00517aa676aa2a971dcf332f6
Kei - Theerawat <theerawat.kei@mail.kmutt.ac.th>
2023-03-18 21:35:01 +0700

    Update Hover on top panel

--- Assets/Script/Gameplay/Profile/TopPanelButton.cs
+++ Assets/Script/Gameplay/Profile/TopPanelButton.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace ArmasCreator.UI
 {
-    public class TopPanelButton : MonoBehaviour
+    public class TopPanelButton : MonoBehaviour , IPointerEnterHandler , IPointerExitHandler
     {
         [SerializeField]
         private GameObject blackOutline;
         [SerializeField]
         private GameObject redOutline;
+        [SerializeField]
+        private List<RectTransform> childObjects;
 
         public void OnSelected()
         {
@@ -22,6 +25,32 @@ namespace ArmasCreator.UI
         {
             redOutline.SetActive(false);
             blackOutline.SetActive(true);
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (redOutline.activeSelf)
+            {
+                return;
+            }
+            SetChildSize(1.2f);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (redOutline.activeSelf)
+            {
+                return;
+            }
+            SetChildSize(1);
+        }
+
+        public void SetChildSize(float childSize)
+        {
+            foreach (RectTransform child in childObjects)
+            {
+                child.localScale = new Vector3 (childSize, childSize , childSize);
+            }
         }
     }
 }
