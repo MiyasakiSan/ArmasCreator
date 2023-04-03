66839cbbb802e8523315b95c8b933351e9b8d124
tewlywth156 <takdanai.thada@gmail.com>
2023-04-04 01:09:05 +0700

    adjust

--- Assets/Script/Gameplay/OntriggerEvent/OnTriggerEvent.cs
+++ Assets/Script/Gameplay/OntriggerEvent/OnTriggerEvent.cs
@@ -24,7 +24,9 @@ namespace ArmasCreator.Gameplay
 
         void OnTriggerEnter(Collider col)
         {
-            if (col.GetComponent<PlayerRpgMovement>().isDead) { return; }
+            if (!col.CompareTag("Player")) { return; }
+
+            if (col.GetComponent<PlayerRpgMovement>()) { return; }
 
             if (onTriggerEnter != null) onTriggerEnter.Invoke(col);
         }
