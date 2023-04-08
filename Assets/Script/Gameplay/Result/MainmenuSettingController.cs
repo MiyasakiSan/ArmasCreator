a0dd8aaf38d611b344466cf577e90a0088c7ce86
Kei - Theerawat <theerawat.kei@mail.kmutt.ac.th>
2023-04-08 18:25:17 +0700

    Add Credit

--- Assets/Script/Gameplay/Result/MainmenuSettingController.cs
+++ Assets/Script/Gameplay/Result/MainmenuSettingController.cs
@@ -9,14 +9,43 @@ namespace ArmasCreator.UI
         [SerializeField]
         private Animator anim;
 
+        public void OnFadeIn()
+        {
+            anim.SetTrigger("First");
+        }
+
+        public void OnFadeOut()
+        {
+            anim.SetTrigger("HideOption");
+            anim.SetTrigger("HideCredit");
+            anim.SetTrigger("Out");
+        }
+
         public void OnShow()
         {
-            anim.SetTrigger("Show");
+            anim.SetTrigger("HideCredit");
+            anim.SetTrigger("ShowOption");
         }
 
         public void OnHide()
         {
-            anim.SetTrigger("Hide");
+            anim.SetTrigger("HideOption");
+        }
+
+        public void OnShowCredit()
+        {
+            anim.SetTrigger("HideOption");
+            anim.SetTrigger("ShowCredit");
+        }
+
+        public void OnHideCredit()
+        {
+            anim.SetTrigger("HideCredit");
+        }
+
+        public void OnResetTrigger(string triggerName)
+        {
+            anim.ResetTrigger(triggerName);
         }
     }
 }
